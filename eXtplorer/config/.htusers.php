c746dd55f1fa0c94521826c98b8a573fd826ddcf
Phil Miesle <phil@fortclever.com>
2015-11-17 13:42:34 +0000

    initial eXtplorer with admin password changed

---
+++ eXtplorer/config/.htusers.php
@@ -0,0 +1,7 @@
+<?php 
+	// ensure this file is being included by a parent file
+	if( !defined( '_JEXEC' ) && !defined( '_VALID_MOS' ) ) die( 'Restricted access' );
+	$GLOBALS["users"]=array(
+	array('admin','$2a$08$84uJD2BFLbnbHAsAS/DiF.MP8EalQISsrRKqrDhfxYED/zSG3l796','/Users/phil/git/EnnisCoderDojo-webhub/ninja','http://localhost','1','','7',1),
+); 
+?>
