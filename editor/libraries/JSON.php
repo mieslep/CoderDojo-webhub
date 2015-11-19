42f5124462f3e454ea5e81ed05e869422ae8514f
Phil Miesle <phil@fortclever.com>
2015-11-19 09:55:18 +0000

    removing further error messages

--- editor/libraries/JSON.php
+++ editor/libraries/JSON.php
@@ -825,7 +825,7 @@ class ext_Json
     function isError($data, $code = null)
     {
         if (class_exists('pear')) {
-            return PEAR::isError($data, $code);
+            return (new PEAR)->isError($data, $code);
         } elseif (is_object($data) && (get_class($data) == 'services_json_error' ||
                                  is_subclass_of($data, 'services_json_error'))) {
             return true;
