20a75574519e27d369168615c3488cf16a9c4b46
Phil Miesle <phil@fortclever.com>
2015-11-24 16:18:35 +0000

    Provide list of Ninja accounts as JSON

---
+++ ninja-list.php
@@ -0,0 +1,30 @@
+<?php 
+$dir          = "ninja";
+
+$return_array = array();
+
+$preurlPieces = explode("/",$_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+array_pop($preurlPieces);
+array_pop($preurlPieces);
+$preurl=implode("/",$preurlPieces);
+$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === true ? 'https:' : 'http:';
+$ninja_url = $protocol."//".$preurl."/ninja/";
+
+
+if(is_dir($dir)){
+
+    if($dh = opendir($dir)){
+        while(($file = readdir($dh)) != false){
+        	if (is_dir($dir."/".$file) and $file != "." and $file != "..") {
+                $return_array[] = array($file => $ninja_url.$file); 
+            }
+        }
+    }
+
+	header('Content-type: application/json');
+    echo json_encode($return_array);
+}
+
+?>
+
+
